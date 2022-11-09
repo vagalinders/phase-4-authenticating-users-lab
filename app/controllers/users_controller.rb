d54764951299e3c831014df277ba95178f95eddf
Chris Mann <chrispmann@gmail.com>
2022-11-09 14:17:11 -0800

    commit tree 9b4cf2087f0265ea12f91897313cdf6bdc9c7a48

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+    def show
+        user = User.find_by(id: session[:user_id])
+        if user
+            render json: user
+        else
+            render json: {error: "not authorized"}, status: :unauthorized
+        end
+    end
+end
